603dbde5c859b1adf478dbaf2976b1adcc3a776f
unknown <andresgime@gmail.com>
2024-05-08 23:05:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Ejercicio 1.1.1. Definir la función media3 tal que (media3 x y z) es la media aritmética de los números x, y y z.
+
+const media3 = (x,y,z) => (x+y+z)/3
+
+/* 
+    Ejercicio 1.2.1. Definir la función sumaMonedas tal que (sumaMonedas a b c d e) es la suma de los euros 
+    correspondientes a a monedas de 1 euro, b de 2 euros, c de 5 euros, d 10 euros y e de 20 euros.
+*/
+
+const sumaMonedas = (a,b,c,d,e) => a*1+b*2+c*5+d*10+e*20
+
+
+
